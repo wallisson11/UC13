9de7319c084a199c43c91f4cee4d3f6f8549ebb8
WALLISSON WILLIAM VITOR MARCELINO <wallisson.wvmarcelin@senacsp.edu.br>
2024-11-29 22:03:39 -0300

    foi feito um get e um post

--- app.js
+++ app.js
@@ -9,6 +9,26 @@ app.get('/',(requisicao, resposta) => {
     resposta.status(200).send(carros2024);
 });
 
+app.get('/:sigla',(requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase();
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Buscando o carro pela Sigla 
+    if (!carro) {
+        // ! Vazio Not
+        resposta
+        .status
+        .send(
+            'Não existe um carro com a sigla informada!' //Mensagem de Carro
+        );
+        return;
+    }
+    resposta.status(200).send(carro); //se encontrado a sigla a resposta correta
+    app.post('/', (req, res) => {
+        const novoCarro = req.body; //Adiciona o corpo enviado para incluir um carro
+        carros2024.push(novoCarro); //Adiciona o novo carro a lista de carros.
+        res.status(200).send(novoCarro); //Retorna o carro adicionado com status 200(ok).
+    })
+})
+
 // Inicia o servidor na porta 3000:
 // http://localhost:3000/
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
--- tabelacarros.js
+++ tabelacarros.js
@@ -17,6 +17,35 @@ const carros2024 = [
         aceleracao: 2.8,
         preco: 330000,
     },
+    {
+        nome: 'BMW',
+        sigla: 'BMW',
+        velocidade: 200,
+        potencia: 650,
+        consumo: 7.2,
+        aceleracao: 3.0,
+        preco: 230000,
+    },
+    {
+        nome: 'Honda',
+        sigla: 'HON',
+        velocidade: 220,
+        potencia: 12.0,
+        consumo: 5.0,
+        aceleracao: 5.8,
+        preco: 11500,
+    },
+    {
+        nome: 'Ford',
+        sigla: 'FOR',
+        velocidade: 250,
+        potencia: 200,
+        consumo: 8.2,
+        aceleracao: 6.8,
+        preco: 12200,
+    },
 ];
+
+
 
 export default carros2024;
